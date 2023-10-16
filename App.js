5ed348763245b099108cc6877f423e5737b5cb4a
Daniel Sládeček <83771369+Daniel1987Daniel@users.noreply.github.com>
2023-10-16 12:02:35 +0200

    Update App.js

--- App.js
+++ App.js
@@ -5,31 +5,6 @@ import NavBar from "./components/NavBar"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 
-class App extends Component {
-  componentDidMount() {
-    $(".name").val()
-  }
-
-  /*
-
-    <dl class="jokes col-6">
-      <dt><a href="#">Lorem ipsum dolor sit amet consectetur adipisicin</a></dt>
-      <dd>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis velit consequuntur tempo</dd>
-    </dl>
-
-    var jokes = $(".jokes")  
-
-    jokes.find("dd").hide()
-    jokes.find("dt").on("click", function() {
-      $(this).next().slideToggle()
-      $(this).siblings("dd").slideUp
-    })
-
-  handleToggle = () => {
-    const el = findDOMNode(this.refs.toggle)
-    $(el).slideToggle()
-  }
-  */
   render() {
     return(
       <div>
